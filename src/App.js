05db7d9124d404972dce75bc5765f144f468ed32
Nikita <nikitau760@gmail.com>
2023-01-13 09:30:22 +0100

    fix problem with local storage and add some style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [tasks, setTasks] = useState([]);
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem("data")));
+    console.log(tasks);
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(tasks));
+  }, [tasks]);
+
+  const removeItem = (posts) => {
+    setTasks(tasks.filter((p) => p.value !== posts.value));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+      <form>
+        <input
+          className="input"
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+          <button
+              type="submit"
+              onClick={(e) => {
+                  if (text) {
+                      e.preventDefault();
+                      setTasks([...tasks, { value: text, complete: { isReady } }]);
+                      setText("");
+                  } else {
+                      alert("you haven`t any task to do");
+                  }
+              }}
+          >
+              {" "}
+              click on me
+          </button>
+      </form>
+
+      <div>
+        {tasks.map((p) => (
+          <div className="ItemCont">
+            <button onClick={() => removeItem(p)}>Delete</button>
+            <div
+              className="taskText"
+              style={
+                p.complete ? { textDecoration: "underline" } : { color: "red" }
+              }
+            >
+              {p.value}
+            </div>
+
+            <button
+              onClick={() => {
+                setIsReady(!isReady);
+                p.complete = isReady;
+              }}
+            >
+              is ready
+            </button>
+          </div>
+        ))}
+      </div>
+    </section>
   );
 }
 
